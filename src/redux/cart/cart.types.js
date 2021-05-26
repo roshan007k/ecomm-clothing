d46d064c8dfc90323d5b670b7b9e062895bb078d
roshan <roshankewat007@gmail.com>
2021-05-26 12:21:14 +0530

    Add item and remove item functionality

--- src/redux/cart/cart.types.js
+++ src/redux/cart/cart.types.js
@@ -1,6 +1,8 @@
 const CartActionTypes={
     TOGGLE_CART_HIDDEN:'TOGGLE_CART-HIDDEN',
-    ADD_ITEM:'ADD_ITEM'
+    ADD_ITEM:'ADD_ITEM',
+    CLEAR_ITEM_CART:'CLEAR_ITEM_CART',
+    REMOVE_ITEM:'REMOVE-ITEM'
 }
 
 export default CartActionTypes;
